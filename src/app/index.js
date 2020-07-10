e6076cadb2c5f2f54282e92bd499a9cf8e82dc24
Vitor Adriano Savedra <v4savedra.dev@outlook.com>
2020-07-10 04:11:41 -0300

    chore: interface updates
    reset css file
    import "Poppins" font-family from google

--- src/app/index.js
+++ src/app/index.js
@@ -2,11 +2,14 @@ import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 
 import Routes from './routes'
+import './style.css'
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes />
+
+      <select name="" id="" />
     </BrowserRouter>
   )
 }

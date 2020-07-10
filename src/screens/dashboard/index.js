8914622770de86a36cb490b576cca7ffe1c81141
Jesiel Sopzak Campos <jesielsopzakcampos@MacBook-Pro-de-Jesiel.local>
2020-07-10 10:11:46 -0300

    Criação do Dashboad

--- src/screens/dashboard/index.js
+++ src/screens/dashboard/index.js
@@ -1,11 +1,58 @@
 import React from 'react'
+import Board from 'react-trello'
+import '../../app/style.css'
 
-const Dashboard = () => {
-  return (
-    <div>
-      <h3>Dashboard</h3>
-    </div>
-  )
+class Dashboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data : {
+        lanes: [
+          {
+            id: 'lane1',
+            title: 'Atividades',
+            cards: [
+              { id: 'Card1', title: 'Criar tela de Login', description: 'Te vira', label: '1 hora' },
+              { id: 'Card2', title: 'Criar dashboard', description: 'Usar o react-trello', label: '3 horas', metadata: { sha: 'be312a1' } },
+              { id: 'Card3', title: 'Criar chat', description: 'Te vira', label: '1 hora' },
+              { id: 'Card4', title: 'Criar profile', description: 'Te vira', label: '1 hora' },
+              { id: 'Card5', title: 'Rankin e pontuação', description: 'Te vira', label: '1 hora' },
+            ]
+          },
+          {
+            id: 'lane2',
+            title: 'Em Andamento',
+            cards: []
+          },
+          {
+            id: 'lane3',
+            title: 'Completo',
+            cards: []
+          }
+        ]
+      }
+    };
+  }
+  onLaneAdd(){
+    alert('tesdte');
+  }
+
+  
+  render() {
+
+    return <div>
+        <div style={}></div>
+        <Board data={this.state.data}   
+            style={{ background: 'linear-gradient(180deg, rgba(43,5,72,1) 0%, rgba(70,48,171,1) 42%, rgba(176,128,255,1) 100%)'}} 
+            editable={true}
+            editLaneTitle={true}
+            laneStyle={{backgroundColor: '#fefefe'}}
+            cardStyle={{backgroundColor: '#ffff99'}}
+          />
+      </div>
+      
+  }
+
 }
 
 export default Dashboard

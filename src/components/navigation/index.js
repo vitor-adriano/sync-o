cd0d41788ba4feb2d6066d4862ada3b1d96c2cfb
Vitor Adriano Savedra <v4savedra.dev@outlook.com>
2020-07-11 11:58:56 -0300

    chore(nav): installed @material-ui/lab package, added speeddial nav

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,10 +1,45 @@
 import React from 'react'
-import { NavLink, useHistory } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
+import { makeStyles } from '@material-ui/core/styles'
+import {
+  AppBar,
+  Toolbar,
+  Button,
+  IconButton,
+  Typography,
+} from '@material-ui/core'
+import SpeedDial from '@material-ui/lab/SpeedDial'
+import SpeedDialIcon from '@material-ui/lab/SpeedDialIcon'
+import SpeedDialAction from '@material-ui/lab/SpeedDialAction'
+import {
+  Dashboard as DashboardIcon,
+  Person as PersonIcon,
+  Chat as ChatIcon,
+  ExitToApp,
+} from '@material-ui/icons'
 
-import './style.css'
+const useStyles = makeStyles(theme => ({
+  speedDial: {
+    position: 'fixed',
+    right: 40,
+    bottom: 100,
+  },
+}))
 
 const Navigation = () => {
   const history = useHistory()
+  const classes = useStyles()
+
+  const [open, setOpen] = React.useState(false)
+  const [hidden, setHidden] = React.useState(false)
+
+  const handleClose = () => {
+    setOpen(false)
+  }
+
+  const handleOpen = () => {
+    setOpen(true)
+  }
 
   const handleLogout = () => {
     localStorage.removeItem('user')
@@ -12,15 +47,42 @@ const Navigation = () => {
   }
 
   return (
-    <nav className="navigation">
-      <div>
-        <NavLink to="/dashboard">Dashboard</NavLink>
-        <NavLink to="/chat">Chat</NavLink>
-        <NavLink to="/profile">Profile</NavLink>
-      </div>
+    <SpeedDial
+      ariaLabel="Menu"
+      className={classes.speedDial}
+      hidden={hidden}
+      icon={<SpeedDialIcon />}
+      onClose={handleClose}
+      onOpen={handleOpen}
+      open={open}
+      direction="up">
+      <SpeedDialAction
+        component={Link}
+        to="/dashboard"
+        icon={<DashboardIcon />}
+        tooltipTitle="Dashboard"
+      />
 
-      <button onClick={handleLogout}>X</button>
-    </nav>
+      <SpeedDialAction
+        component={Link}
+        to="/profile"
+        icon={<PersonIcon />}
+        tooltipTitle="Perfil"
+      />
+
+      <SpeedDialAction
+        component={Link}
+        to="/chat"
+        icon={<ChatIcon />}
+        tooltipTitle="Chat"
+      />
+
+      <SpeedDialAction
+        icon={<ExitToApp />}
+        tooltipTitle="Sair"
+        onClick={handleLogout}
+      />
+    </SpeedDial>
   )
 }
 
